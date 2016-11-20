418241dac512fbc41c8128d2d236b8df8f2f0a0f
Jared Thomas <jaredthomas@Jareds-MacBook-Air.local>
2016-11-20 07:15:59 -0800

    arrays udemy

---
+++ UdemyArray.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class UdemyArray {
+
+    static void arrays() {
+        System.out.println("\nInside arrays ...");
+        int[] myArray = new int[]{9, 11, 2, 5, 4, 4, 6};
+        System.out.println("myArray.length " + myArray.length);
+        System.out.println("myArray[1] " + myArray[1]);
+        System.out.println("myArray[7] " + myArray[7]);
+
+    }
+
+    public static void main(String[] args){
+        arrays();
+    }
+
+}
